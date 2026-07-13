8541076fee864494413001d17ff9913ddc819c8d
joharbaru <81684135+gamailmy@users.noreply.github.com>
2026-07-13 18:43:20 +0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -64,7 +64,7 @@ export default function Login() {
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                   className="pl-10 w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
-                  placeholder="admin"
+                  placeholder="***"
                 />
               </div>
             </div>
@@ -81,7 +81,7 @@ export default function Login() {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   className="pl-10 w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all"
-                  placeholder="admin123"
+                  placeholder="***"
                 />
               </div>
             </div>
