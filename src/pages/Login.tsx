ff35441a24bef445820cb9bab0af8201d2773600
joharbaru <81684135+gamailmy@users.noreply.github.com>
2026-07-13 19:03:52 +0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -36,7 +36,7 @@ export default function Login() {
           </div>
         )}
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Masuk ke FakturPro
+          Masuk ke Faktur TindonPay
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
           atau masuk sebagai <span className="font-medium text-blue-600">admin</span> (Pass: admin123)
